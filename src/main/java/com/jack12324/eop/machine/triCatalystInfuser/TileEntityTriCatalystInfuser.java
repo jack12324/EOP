61b9efe2b74d1d933fefe3bf8fa459137e225c5e
jack12324 <jacksporter24@gmail.com>
2017-08-11 20:13:49 -0500

    Catalyst infuser smarter input
    in isItemValid now checks if there is a full input slot with the item. this should prevent machines from filling up with one input item

--- src/main/java/com/jack12324/eop/machine/triCatalystInfuser/TileEntityTriCatalystInfuser.java
+++ src/main/java/com/jack12324/eop/machine/triCatalystInfuser/TileEntityTriCatalystInfuser.java
@@ -1,9 +1,13 @@
 package com.jack12324.eop.machine.triCatalystInfuser;
 
+import com.jack12324.eop.item.ModItems;
 import com.jack12324.eop.machine.TEFluidProducer;
+import com.jack12324.eop.recipe.RecipeHandler;
 import com.jack12324.eop.recipe.RecipeHolder;
 import com.jack12324.eop.recipe.recipeInterfaces.EOPRecipe;
 import com.jack12324.eop.util.InventorySlotHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 
 import java.util.ArrayList;
@@ -30,5 +34,31 @@ public class TileEntityTriCatalystInfuser extends TEFluidProducer {
                 this.sideIO[index] = 3;
             else this.sideIO[index] = val + 1;
         }
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        if (this.slotHelper.getUpgradeSlotIndex(0) == index && stack.getItem() != ModItems.speedUpgrade)
+            return false;
+        if (this.slotHelper.getUpgradeSlotIndex(1) == index && stack.getItem() != ModItems.energyUpgrade)
+            return false;
+
+        for (int indexes : this.slotHelper.getIn()) {
+            if (index == indexes && !RecipeHandler.getInItems(this.getRecipeList()).contains(stack.getItem()) && !hasFullStackOfItem(stack.getItem()))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean hasFullStackOfItem(Item item) {
+        ItemStack[] stacks = new ItemStack[this.slotHelper.getInSlotSize()];
+        for (int i = 0; i < stacks.length; i++)
+            stacks[i] = this.slots.getStackInSlot(this.slotHelper.getInSlotIndex(i));
+
+        for (ItemStack stack : stacks) {
+            if (stack.getItem() == item && stack.getCount() >= stack.getItem().getItemStackLimit(stack))
+                return true;
+        }
+        return false;
     }
 }
